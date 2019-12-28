0b060e14e14a16d2b888ab1a2f07b9c276a9aa78
ARBANI OUMAIMA <oumaima.arbani@gmail.com>
2019-12-28 18:45:26 +0100

    Add more views

--- XamarinProject/XamarinProject/Home.xaml.cs
+++ XamarinProject/XamarinProject/Home.xaml.cs
@@ -16,5 +16,26 @@ namespace XamarinProject
         {
             InitializeComponent();
         }
+
+        private void absence(object sender, EventArgs e)
+        {
+
+        }
+        async private void student(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new addStudent());
+        }
+        private void lesson(object sender, EventArgs e)
+        {
+
+        }
+        private void search(object sender, EventArgs e)
+        {
+
+        }
+        private void logout(object sender, EventArgs e)
+        {
+
+        }
     }
 }
